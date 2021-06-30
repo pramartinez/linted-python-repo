5d0dc480d26d093ce4bf215fd56bab8b5ff3d47e
Práxedes Martínez Moreno <pramtnez@gmail.com>
2021-06-30 19:34:22 +0200

    Añadiendo comentarios.

--- fuu/core.py
+++ fuu/core.py
@@ -4,12 +4,21 @@ Implements a boilerplate clas
 
 
 class ShoppingList:
+    '''
+    ShoppingList class. It saves a list of items and how many there are
+    '''
     def __init__(self, *args):
         self.items = args
         self.nitems = len(args)
 
     def args(self):
+        '''
+        Getter 
+        '''
         return self.items
 
     def first_arg(self):
+        '''
+        First elements of the shopping list
+        '''
         return self.items[0]
